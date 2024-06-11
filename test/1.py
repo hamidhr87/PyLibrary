7a88d1f2301ee986ea13e6b555d3123135ef3da0
HamidReza Horr <hamidreza.hr87@gmail.com>
2024-06-11 23:39:50 +0330

    5th

--- test/1.py
+++ test/1.py
@@ -1,8 +1,8 @@
-from PyLibrary.PyLibrary import PyLibrary
+# from PyLibrary.PyLibrary import PyLibrary
 
-obj = PyLibrary("f.taheri", "1234")
-content = obj.download_file("domain1","hamid","3.txt")
-with open("3.txt","wb") as file:
-    file.write(content)
-message = obj.upload_file("domain1","hamid","bucket","33.txt")
-print(message)
+# obj = PyLibrary("f.taheri", "1234")
+# content = obj.download_file("domain1","hamid","3.txt")
+# with open("3.txt","wb") as file:
+#     file.write(content)
+# message = obj.upload_file("domain1","hamid","bucket","33.txt")
+# print(message)
