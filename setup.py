60caca3afeda160fe83dc52620533d440890ae2b
HamidReza Horr <hamidreza.hr87@gmail.com>
2024-06-11 23:59:25 +0330

    8th

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "PyLibrary",
+    name = "pylibrary",
     version = "0.0.1",
     author = "Hamid Reza Horr",
     author_email = "hamidreza.hr87@gmail.com",
